d613ab54ee28738ecba9c422f92d368ef984092b
24bct113 <24bct113@kgcas.com>
2025-07-12 10:50:20 +0530

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-var number=15;
+var number=12;
 var name='js';
 var iscloudy=true;
+
 console.log(number+" "+name+" "+iscloudy);
+document.write(number);
+document.write(name);
+document.write(iscloudy);
+
+//task 2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task 3
+var num1=5;
+var num2=10;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
